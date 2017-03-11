edb7ae248be418c6a29d39b9786aee89867fbb7e
rakesh <rak1994kumar@gmail.com>
2017-03-11 17:53:57 +0530

    dasdasdasdasd

---
+++ src/main/java/com/mathologic/projects/controllers/UserPlanController.java
@@ -0,0 +1,44 @@
+package com.mathologic.projects.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mathologic.projects.models.UserPlan;
+import com.mathologic.projects.repository.UserPlanRepository;
+
+@RestController
+@RequestMapping("/api/v1/userPlan")
+public class UserPlanController {
+	@Autowired
+	UserPlanRepository userPlanRepository;
+	
+	public UserPlanController() {
+	}
+
+	@RequestMapping(value = "/createPlan", method = RequestMethod.POST)
+	public String createPlan(@RequestBody UserPlan userPlan) {
+		if (userPlan != null) {
+			userPlanRepository.save(userPlan);
+		}
+		return "Plan created Successfully";
+	}
+
+	@RequestMapping(value = "/getPlan", method = RequestMethod.GET)
+	public Page<UserPlan> findByAllParams(@RequestParam(value = "planname", required = true)String planname) {
+		
+		 Page<UserPlan> userPlanList = userPlanRepository.findByAllUserPlanParams(planname, createPageRequest());
+		 return userPlanList;
+		
+	}
+	private Pageable createPageRequest() {
+	    return new PageRequest(0, 10, Sort.Direction.ASC, "planName");
+	}
+}
---
+++ src/main/java/com/mathologic/projects/repository/UserPlanRepository.java
@@ -0,0 +1,19 @@
+package com.mathologic.projects.repository;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.mathologic.projects.models.UserPlan;
+
+@RepositoryRestResource
+public interface UserPlanRepository extends JpaRepository<UserPlan, Long> {
+	
+	@Query("SELECT up from UserPlan as up WHERE (up.planName LIKE %:planname% OR :planname IS NULL OR planname is '%%')")
+	Page<UserPlan> findByAllUserPlanParams(@Param("planname") String planname,Pageable pagable);
+
+}
