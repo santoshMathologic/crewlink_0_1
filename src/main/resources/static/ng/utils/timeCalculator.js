00a1c796eb334f6cd3b05cbd27b491a6f9e3079e
rakesh <rak1994kumar@gmail.com>
2017-03-20 13:02:15 +0530

    dadsdas

--- src/main/resources/static/ng/utils/timeCalculator.js
+++ src/main/resources/static/ng/utils/timeCalculator.js
@@ -1,4 +1,3 @@
-
 /**
  *  Author @santosh 
  *  TimeCalculation for Crewlink Decision Support Support System
@@ -6,14 +5,43 @@
  * 
  */
 
+function cal_differenceBetweenTwoTimes(startTime, startDay, endTime, endDay) {
 
+	/**
+	 *    All Variable Calculation
+	 * 
+	 */
+	var totalDuration = 0;
+	var Days = ['SUNDAY','MONDAY','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY'];
 
-  function cal_differenceBetweenTwoTimes(startTime,startDay,endDay,endTime){
-	  
-	  console.log(startTime);
-	  console.log(startDay);
-	  console.log(endDay);
-	  console.log(endTime);
-	  
-	  
-  }
+	dep = startTime.split(':');
+	var dH = $.trim(dep[0]);
+	var dM = $.trim(dep[1]);
+
+	console.log("" + dH + "" + dM);
+
+	arr = endTime.split(":");
+	var aH = $.trim(arr[0]);
+	var aM = $.trim(arr[1]);
+
+	console.log("" + aH + " " + aM);
+
+	var depMin = parseInt(dH * 60) + parseInt(dM);
+	var arrMin = parseInt(aH * 60) + parseInt(aM);
+
+	var diff_in_Day = startDay - endDay;
+	if (diff_in_Day >= 0) {
+		totalDuration = diff_in_Day * 24 * 60 + 24 * 60 - depMin + arrMin;
+	} else if (diff_in_Day == -1) {
+		totalDuration = (arrMin - depMin);
+	} else {
+		diff_in_Day = Days.length - 1 - startDay + endDay;
+		totalDuration = diff_in_Day * 24 * 60 + 24 * 60 - depMin
+				+ arrMin;
+
+	}
+	
+
+	return totalDuration;
+
+}
